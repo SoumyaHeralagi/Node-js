fe988cd3c5d08fabf33634ff8748e766507f18ab
Soumya <heralagisoumya@gmail.com>
2022-06-25 15:35:46 +0530

    commit

---
+++ async_dir.js
@@ -0,0 +1,12 @@
+var fs=require('fs');
+
+fs.mkdir('stuff',function(){
+    fs.readFile('readMe.txt','utf8',function(err,data){
+        fs.writeFile('./stuff/writeMe.txt', data);
+    });
+});
+
+
+fs.unlink('./stuff/writeMe.txt',function(){
+    fs.rmdir('stuff');
+ });
