e5ce640eafebdefdd8599b03a59b5f0d40c4aa68
Soumya <heralagisoumya@gmail.com>
2022-06-25 10:39:52 +0530

    commit

---
+++ mod_pat1.js
@@ -0,0 +1,14 @@
+var counter=function(arr){
+    return 'there are '+ arr.length +' elements in the array';
+};
+
+var adder=function(a,b){
+    return `sum of two numbers is ${a+b}`;
+
+};
+
+var pi=3.142
+
+module.exports.counter=counter;
+module.exports.adder=adder;
+module.exports.pi=pi;
---
+++ mod_pat2.js
@@ -0,0 +1,5 @@
+var mod_pat1=require('./mod_pat1');
+
+console.log(mod_pat1.counter(['a','b','c']));
+console.log(mod_pat1.adder(5,6));
+console.log(mod_pat1.adder(mod_pat1.pi,5));
